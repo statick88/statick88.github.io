b1beaf87a87c90a7dff4fe0d30bb0501c67a1289
Diego Saavedra <dsaavedra88@gmail.com>
2024-10-10 14:25:01 -0500

    Configurar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://statick88.github.io', // Cambia <tu-usuario> por tu nombre de usuario de GitHub
+  base: '/statick88.github.io', // Asegúrate de que esta sea la ruta correcta
+});
+
+
