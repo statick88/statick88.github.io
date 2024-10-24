34c97e4de8e92f3c79ea6144046f06fc74ebddd9
Diego Saavedra <dsaavedra88@gmail.com>
2024-10-24 07:56:52 -0500

    add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://statick88.github.io',
+  base: 'https://statick88.github.io',
+})
